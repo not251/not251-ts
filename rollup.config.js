857b1c47ea99d8aaa6df48805b3061f7136efe45
Alessandro Petrone <alessandro.nge@gmail.com>
2024-09-27 18:54:05 +0200

    fix build process

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+const typescript = require("@rollup/plugin-typescript");
+const resolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+
+module.exports = [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.esm.js",
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({
+        tsconfig: "./tsconfig.json",
+        declaration: true,
+        declarationDir: "dist",
+      }),
+      resolve(),
+      commonjs(),
+    ],
+  },
+];
