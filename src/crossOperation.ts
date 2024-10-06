7815471b9f38fbae10d96aec0219c25b2f708b12
Davide Fumai <141966267+fumaidavide@users.noreply.github.com>
2024-10-06 15:07:35 +0200

    commenti pigri

--- src/crossOperation.ts
+++ src/crossOperation.ts
@@ -1,6 +1,9 @@
 import positionVector from "./positionVector";
 import intervalVector from "./intervalVector";
 
+// Converts an IntervalVector into a PositionVector. 
+// Starts from an initial offset value and iteratively sums the intervals, storing cumulative sums in the out array to represent the resulting positions. 
+// The final sum, adjusted by the initial offset, determines the span for the new PositionVector.
 export function toPositions(s: intervalVector): positionVector {
   let n = s.data.length;
   let out = new Array(n);
@@ -13,6 +16,9 @@ export function toPositions(s: intervalVector): positionVector {
   return new positionVector(out, s.modulo, sum);
 }
 
+// Converts a PositionVector into an IntervalVector. 
+// Calculates the interval between consecutive elements in the data array by finding the difference between each element and its successor. 
+// These differences are stored in the out array as intervals, which form the IntervalVector. The first element of data sets the initial offset.
 export function toIntervals(s: positionVector): intervalVector {
   let out: number[] = [];
   let n = s.data.length;
@@ -25,6 +31,9 @@ export function toIntervals(s: positionVector): intervalVector {
   return new intervalVector(out, s.modulo, s.data[0]);
 }
 
+// Constructs a PositionVector by selecting elements from an existing PositionVector (s) based on cumulative intervals provided by an IntervalVector (j). 
+// The selection starts from the offset in j, with subsequent positions determined by iteratively adding intervals from j to this sum. 
+// The resulting vector is adjusted to cover its range via spanUpdate.
 export function selectFromInterval(s: positionVector, j: intervalVector) {
   let v: number[] = [];
   let sum = j.offset;
