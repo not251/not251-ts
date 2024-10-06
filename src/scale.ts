c54a913130e3868214faba2fac2d52dc120502f4
Davide Fumai <141966267+fumaidavide@users.noreply.github.com>
2024-10-06 15:18:29 +0200

    commenti pigri

--- src/scale.ts
+++ src/scale.ts
@@ -2,6 +2,20 @@ import positionVector from "./positionVector";
 import intervalVector from "./intervalVector";
 import { toPositions } from "./crossOperation";
 
+// scale Generates a positionVector based on an input intervalVector, allowing for transformations like inversion and mirroring. 
+// It rotates the interval vector by modo, adjusts the root, and optionally inverts or mirrors it. 
+// The output is then converted to a position vector, which is roto-translated by grado to finalize its configuration.
+
+// Parameters:
+// intervalli: The base intervals that define the scale.
+// root: Starting pitch or offset for the scale.
+// modo: Rotation step to define the starting position of the scale.
+// grado: Degree of roto-translation applied to the final scale.
+// isInvert: If true, the scale is inverted.
+// isMirror: If true, a mirroring operation is applied.
+// mirrorPos: Position at which the mirroring occurs.
+// mirrorLeft: Determines the mirroring direction (left or right).
+
 export default function scale(
   intervalli: intervalVector,
   root: number = 0,
