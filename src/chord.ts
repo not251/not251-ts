56fca1daed711100e349afad18476a4e8fb2099a
Davide Fumai <141966267+fumaidavide@users.noreply.github.com>
2024-10-06 15:20:37 +0200

    commenti pigri

--- src/chord.ts
+++ src/chord.ts
@@ -2,6 +2,19 @@ import positionVector from "./positionVector";
 import intervalVector from "./intervalVector";
 import { selectFromInterval } from "./crossOperation";
 
+// chordFromPosition Generates a chord by selecting positions from a scale and applying transformations, including inversion and negation. 
+// This function uses a positionVector scale and selection to produce a position vector chord.
+
+// Parameters:
+// scala: The scale represented as a positionVector.
+// grado: Degree of rotation to change the starting note of the scale.
+// selection: Determines which positions in the scale are included in the chord.
+// preVoices and postVoices: Number of voices to consider before and after the selection.
+// isInvert: If true, the chord is inverted.
+// isNegative: If true, the chord undergoes a negation transformation.
+// negativePos: Position for the negation operation.
+// standardNegative: Defines the type of negation applied.
+// root and octave: Adjust the root note and octave of the resulting chord.
 export function chordFromPosition(
   scala: positionVector,
   grado: number = 0,
@@ -31,6 +44,19 @@ export function chordFromPosition(
   return out;
 }
 
+// Similar to chordFromPosition, this function constructs a chord but uses intervals rather than positions for the selection process. 
+// It applies rotation, inversion, and negative to the generated position vector based on the provided scale.
+
+// Parameters:
+// scala: The scale as a positionVector.
+// grado: Degree of rotation to change the starting note.
+// selection: Intervals that define which positions in the scale are selected.
+// preVoices and postVoices: Number of voices considered before and after the selection.
+// isInvert: If true, the chord is inverted.
+// isNegative: If true, the chord undergoes a negative transformation.
+// negativePos: Position for negative.
+// standardNegative: Specifies the negative type.
+// root and octave: Adjust the root and octave of the chord.
 export function chordFromInterval(
   scala: positionVector,
   grado: number = 0,
