f6668f6ed8267a3af35f8ad0e45a3063099d0a8d
Davide Fumai <141966267+fumaidavide@users.noreply.github.com>
2024-10-09 17:01:17 +0200

    commenti belli pigri

--- src/chord.ts
+++ src/chord.ts
@@ -2,19 +2,25 @@ import positionVector from "./positionVector";
 import intervalVector from "./intervalVector";
 import { selectFromInterval } from "./crossOperation";
 
-// chordFromPosition Generates a chord by selecting positions from a scale and applying transformations, including inversion and negation. 
-// This function uses a positionVector scale and selection to produce a position vector chord.
-
-// Parameters:
-// scala: The scale represented as a positionVector.
-// grado: Degree of rotation to change the starting note of the scale.
-// selection: Determines which positions in the scale are included in the chord.
-// preVoices and postVoices: Number of voices to consider before and after the selection.
-// isInvert: If true, the chord is inverted.
-// isNegative: If true, the chord undergoes a negation transformation.
-// negativePos: Position for the negation operation.
-// standardNegative: Defines the type of negation applied.
-// root and octave: Adjust the root note and octave of the resulting chord.
+/**
+ * Generates a chord by selecting positions from a scale and applying transformations, 
+ * including inversion and negation. This function uses a positionVector scale and selection 
+ * to produce a position vector chord.
+ * 
+ * @param scala - The scale represented as a positionVector.
+ * @param grado - Degree of rotation to change the starting note of the scale (default is 0).
+ * @param selection - Determines which positions in the scale are included in the chord.
+ * @param preVoices - Number of voices to consider before the selection (default is 3).
+ * @param position - Degree of roto-translation applied to the selected chord (default is 0).
+ * @param postVoices - Number of voices to consider after the selection (default is 3).
+ * @param isInvert - If true, the chord is inverted (default is false).
+ * @param isNegative - If true, the chord undergoes a negation transformation (default is false).
+ * @param negativePos - Position for the negation operation (default is 10).
+ * @param standardNegative - Defines the type of negation applied (default is true).
+ * @param root - Adjusts the root note of the resulting chord (default is 0).
+ * @param octave - Adjusts the octave of the resulting chord (default is 4).
+ * @returns The resulting positionVector chord after applying transformations.
+ */
 export function chordFromPosition(
   scala: positionVector,
   grado: number = 0,
@@ -44,19 +50,25 @@ export function chordFromPosition(
   return out;
 }
 
-// Similar to chordFromPosition, this function constructs a chord but uses intervals rather than positions for the selection process. 
-// It applies rotation, inversion, and negative to the generated position vector based on the provided scale.
-
-// Parameters:
-// scala: The scale as a positionVector.
-// grado: Degree of rotation to change the starting note.
-// selection: Intervals that define which positions in the scale are selected.
-// preVoices and postVoices: Number of voices considered before and after the selection.
-// isInvert: If true, the chord is inverted.
-// isNegative: If true, the chord undergoes a negative transformation.
-// negativePos: Position for negative.
-// standardNegative: Specifies the negative type.
-// root and octave: Adjust the root and octave of the chord.
+/**
+ * Constructs a chord using intervals rather than positions for the selection process. 
+ * It applies rotation, inversion, and negation to the generated position vector based 
+ * on the provided scale.
+ * 
+ * @param scala - The scale as a positionVector.
+ * @param grado - Degree of rotation to change the starting note (default is 0).
+ * @param selection - Intervals that define which positions in the scale are selected.
+ * @param preVoices - Number of voices considered before the selection (default is 3).
+ * @param position - Degree of roto-translation applied to the selected chord (default is 0).
+ * @param postVoices - Number of voices considered after the selection (default is 3).
+ * @param isInvert - If true, the chord is inverted (default is false).
+ * @param isNegative - If true, the chord undergoes a negative transformation (default is false).
+ * @param negativePos - Position for negation (default is 10).
+ * @param standardNegative - Specifies the type of negation applied (default is true).
+ * @param root - Adjusts the root note of the chord (default is 0).
+ * @param octave - Adjusts the octave of the chord (default is 4).
+ * @returns The resulting positionVector chord after applying transformations.
+ */
 export function chordFromInterval(
   scala: positionVector,
   grado: number = 0,
