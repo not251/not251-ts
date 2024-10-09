0e4055d003a06009acaf8b0b840e4976cc5e6130
Davide Fumai <141966267+fumaidavide@users.noreply.github.com>
2024-10-09 17:00:04 +0200

    commenti parecchio pigri

--- src/scale.ts
+++ src/scale.ts
@@ -2,19 +2,21 @@ import positionVector from "./positionVector";
 import intervalVector from "./intervalVector";
 import { toPositions } from "./crossOperation";
 
-// Generates a positionVector based on an input intervalVector, allowing for transformations like inversion and mirroring. 
-// It rotates the interval vector by modo, adjusts the root, and optionally inverts or mirrors it. 
-// The output is then converted to a position vector, which is roto-translated by grado to finalize its configuration.
-
-// Parameters:
-// intervalli: The base intervals that define the scale.
-// root: Starting pitch or offset for the scale.
-// modo: Rotation step to define the starting position of the scale.
-// grado: Degree of roto-translation applied to the final scale.
-// isInvert: If true, the scale is inverted.
-// isMirror: If true, a mirroring operation is applied.
-// mirrorPos: Position at which the mirroring occurs.
-// mirrorLeft: Determines the mirroring direction (left or right).
+/**
+ * Generates a positionVector based on an input intervalVector, allowing for transformations like inversion and mirroring. 
+ * It rotates the interval vector by modo, adjusts the root, and optionally inverts or mirrors it. 
+ * The output is then converted to a position vector, which is roto-translated by grado to finalize its configuration.
+ * 
+ * @param intervalli - The base intervals that define the scale.
+ * @param root - Starting pitch or offset for the scale (default is 0).
+ * @param modo - Rotation step to define the starting position of the scale (default is 0).
+ * @param grado - Degree of roto-translation applied to the final scale (default is 0).
+ * @param isInvert - If true, the scale is inverted (default is false).
+ * @param isMirror - If true, a mirroring operation is applied (default is false).
+ * @param mirrorPos - Position at which the mirroring occurs (default is 0).
+ * @param mirrorLeft - Determines the mirroring direction (left or right) (default is false).
+ * @returns The resulting positionVector after applying the transformations.
+ */
 
 export default function scale(
   intervalli: intervalVector,
