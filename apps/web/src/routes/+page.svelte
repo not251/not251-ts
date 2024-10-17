7095c9f854ad8cb803754e6491a17a2c6aa6474f
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-10-17 18:49:01 +0200

    feat: keyboard on home page

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-	import Chord from '$lib/components/not251/Chord.svelte';
 	import Scale from '$lib/components/not251/Scale.svelte';
+	import Chord from '$lib/components/not251/Chord.svelte';
+	import Keyboard from '$lib/components/keyboard/Keyboard.svelte';
+
+	import { chord } from '$lib/components/not251/store';
 </script>
 
 <div class="flex w-full flex-col items-center justify-center gap-10 py-10">
 	<h1 class="text-center text-4xl font-semibold">not251 Web Demo</h1>
-	<div class="flex w-full flex-row items-center justify-center gap-10">
+	<div class="w-full">
+		<Keyboard middleC={64} octaves={$chord.options.octave[0]} keysPressed={$chord.notes.data} />
+	</div>
+	<div class="flex w-full flex-row items-start justify-center gap-10">
 		<Scale />
 		<Chord />
 	</div>
