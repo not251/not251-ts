b7b97263a44c18907d849c482a6da288dee81e47
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-10-18 12:54:37 +0200

    fix: conform types with `undefined` except for number arrays that can actually be empty

--- apps/web/src/lib/components/not251/types.ts
+++ apps/web/src/lib/components/not251/types.ts
@@ -2,29 +2,29 @@ import type { intervalVector, positionVector } from '@not251/not251';
 
 export type ScaleType = {
 	options: {
-		intervals: intervalVector;
+		intervals?: intervalVector;
 		modo: number[];
 		grado: number[];
 		root: number[];
-		isInvert: boolean;
-		isMirror: boolean;
-		mirrorLeft: boolean;
+		isInvert?: boolean;
+		isMirror?: boolean;
+		mirrorLeft?: boolean;
 		mirrorPos: number[];
 	};
-	notes: positionVector;
+	notes?: positionVector;
 };
 
 export type ChordType = {
 	options: {
-		scale: positionVector;
+		scale?: positionVector;
 		octave: number[];
 		preVoices: number[];
 		position: number[];
 		postVoices: number[];
-		isInvert: boolean;
-		isNegative: boolean;
-		standardNegative: boolean;
+		isInvert?: boolean;
+		isNegative?: boolean;
+		standardNegative?: boolean;
 		negativePos: number[];
 	};
-	notes: positionVector;
+	notes?: positionVector;
 };
