00a492611d94a52feec0096cd8d17c1c115fd0f7
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-10-20 13:02:07 +0200

    chore: remove `web` package (svelte)

--- apps/web/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import Not251 from '$lib/components/not251/not251.svelte';
-</script>
-
-<div class="flex w-full flex-col items-center justify-center gap-10 py-10">
-	<h1 class="text-center text-4xl font-semibold">not251 Web Demo</h1>
-	<Not251 />
-</div>
