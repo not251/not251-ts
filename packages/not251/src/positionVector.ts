f6a32dca112d98f484dcf3916ea85e41e2009e50
Linkcrimson <dbbellino@gmail.com>
2024-11-29 18:58:03 +0100

    Fixed version of degreeFunction

--- packages/not251/src/positionVector.ts
+++ packages/not251/src/positionVector.ts
@@ -551,28 +551,26 @@ for (let i = 0; i < index_chord.data.length; i++) {
     let baseChord = new Set<number>();
     baseChord.add(0);
 
-    // Determines the main degrees (third, fifth, seventh)
+  // Determines the main degrees (third, fifth, seventh)
     for (let i = 1; i < shiftedData.length; i++) {
-      switch (shiftedData[i]) {
-        case 4:
-          degFunc[i] = 2;
-          baseChord.add(4);
-          intervalTypes.add("3maj");
-          break;
-        case 7:
-          degFunc[i] = 4;
-          baseChord.add(7);
-          intervalTypes.add("5");
-          break;
-        case 11:
-          degFunc[i] = 6;
-          baseChord.add(11);
-          intervalTypes.add("7maj");
-          break;
+      if (shiftedData[i] == 4) {
+        degFunc[i] = 2;
+        baseChord.add(4);
+        intervalTypes.add("3maj");
+      }
+      if (shiftedData[i] == 7) {
+        degFunc[i] = 4;
+        baseChord.add(7);
+        intervalTypes.add("5");
+      }
+      if (shiftedData[i] == 11) {
+        degFunc[i] = 6;
+        baseChord.add(11);
+        intervalTypes.add("7maj");
       }
     }
 
-    // Searches for alternative intervals if the main ones are not present
+  // Searches for alternative intervals if the main ones are not present
     if (!intervalTypes.has("3maj")) {
       for (let i = 1; i < shiftedData.length; i++) {
         if (shiftedData[i] == 3) {
@@ -584,22 +582,55 @@ for (let i = 0; i < index_chord.data.length; i++) {
       }
     }
 
-    if (!intervalTypes.has("5")) {
+    // If perfect fifth is not found and third is present, look for augmented fifth
+    if (!intervalTypes.has("5") && intervalTypes.has("3maj")) {
       for (let i = 1; i < shiftedData.length; i++) {
         if (shiftedData[i] == 8) {
           degFunc[i] = 4;
           baseChord.add(8);
           intervalTypes.add("5aug");
           break;
-        } else if (shiftedData[i] == 6) {
+        }
+      }
+    }
+
+    // If neither major nor minor third is found, look for second as a substitute
+    if (!intervalTypes.has("3maj") && !intervalTypes.has("3min")) {
+      for (let i = 1; i < shiftedData.length; i++) {
+        if (shiftedData[i] == 2) {
+          degFunc[i] = 2;
+          baseChord.add(2)
+          intervalTypes.add("3dim");
+          break;
+        }
+      }
+    }
+
+    // If third is not found, look for a fourth as a substitute for the third
+    if (!intervalTypes.has("3maj") && !intervalTypes.has("3min") && !intervalTypes.has("3dim")) {
+      for (let i = 1; i < shiftedData.length; i++) {
+        if (shiftedData[i] == 5) {
+          degFunc[i] = 2;
+          baseChord.add(5)
+          intervalTypes.add("3aug");
+          break;
+        }
+      }
+    }
+
+    // Look for diminished fifth
+    if (!intervalTypes.has("5") && !intervalTypes.has("5aug") && !intervalTypes.has("3maj")) {
+      for (let i = 1; i < shiftedData.length; i++) {
+        if (shiftedData[i] == 6) {
           degFunc[i] = 4;
-          baseChord.add(6);
+          baseChord.add(6)
           intervalTypes.add("5dim");
           break;
         }
       }
     }
 
+    // Look for minor seventh
     if (!intervalTypes.has("7maj")) {
       for (let i = 1; i < shiftedData.length; i++) {
         if (shiftedData[i] == 10) {
@@ -607,9 +638,16 @@ for (let i = 0; i < index_chord.data.length; i++) {
           baseChord.add(10);
           intervalTypes.add("7min");
           break;
-        } else if (shiftedData[i] == 9) {
+        }
+      }
+    }
+
+    // Look for diminished seventh (major sixth)
+    if (!intervalTypes.has("7maj") && !intervalTypes.has("7min")) {
+      for (let i = 1; i < shiftedData.length; i++) {
+        if (shiftedData[i] == 9) {
           degFunc[i] = 6;
-          baseChord.add(9);
+          baseChord.add(9)
           intervalTypes.add("7dim");
           break;
         }
@@ -625,6 +663,8 @@ for (let i = 0; i < index_chord.data.length; i++) {
             intervalTypes.add("2min");
           } else if (shiftedData[i] == 2) {
             intervalTypes.add("2");
+          } else if (shiftedData[i] == 3) {
+            intervalTypes.add("2aug")
           }
         } else if (shiftedData[i] > 4 && shiftedData[i] < 7) {
           degFunc[i] = 3;
