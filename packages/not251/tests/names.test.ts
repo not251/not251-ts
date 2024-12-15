39a8653d234377449c67deb4bbc18af95f24c887
Alessandro Petrone <alessandro.nge@gmail.com>
2024-12-15 19:16:22 +0100

    updated tests

--- packages/not251/tests/names.test.ts
+++ packages/not251/tests/names.test.ts
@@ -3,25 +3,25 @@ import * as not251 from "../src";
 describe("scaleNames tests", () => {
   it("Do maggiore", () => {
     const doMaggiore = not251.scale();
-    const nomi = doMaggiore.names("it").map((note) => note.name);
+    const nomi = not251.names(doMaggiore, ["it"]).map((note) => note.it);
     expect(nomi).toEqual(["Do", "Re", "Mi", "Fa", "Sol", "La", "Si"]);
   });
 
   it("Re maggiore", () => {
     const reMaggiore = not251.scale({ root: 2 });
-    const nomi = reMaggiore.names("it").map((note) => note.name);
-    expect(nomi).toEqual(["Re", "Mi", "Fa♯", "Sol", "La", "Si", "Do♯"]);
+    const nomi = not251.names(reMaggiore, ["it"]).map((note) => note.it);
+    expect(nomi).toEqual(["Re", "Mi", "Fa#", "Sol", "La", "Si", "Do#"]);
   });
   /*
   it("Mi♭ maggiore", () => {
     const mibMaggiore = not251.scale({ root: 3 });
-    const nomi = mibMaggiore.names("it").map((note) => note.name);
+    const nomi = not251.names(mibMaggiore, ["it"]).map((note) => note.it);
     expect(nomi).toEqual(["Mi♭", "Fa", "Sol", "La♭", "Si♭", "Do", "Re"]);
   });
 */
   it("Scala minore naturale di La", () => {
     const laMinoreNaturale = not251.scale({ grado: 5 });
-    const nomi = laMinoreNaturale.names("it").map((note) => note.name);
+    const nomi = not251.names(laMinoreNaturale, ["it"]).map((note) => note.it);
     expect(nomi).toEqual(["La", "Si", "Do", "Re", "Mi", "Fa", "Sol"]);
   });
 
@@ -31,8 +31,8 @@ describe("scaleNames tests", () => {
       12,
       12
     );
-    const nomi = laMinoreArmonica.names("it").map((note) => note.name);
-    expect(nomi).toEqual(["La", "Si", "Do", "Re", "Mi", "Fa", "Sol♯"]);
+    const nomi = not251.names(laMinoreArmonica, ["it"]).map((note) => note.it);
+    expect(nomi).toEqual(["La", "Si", "Do", "Re", "Mi", "Fa", "Sol#"]);
   });
 
   it("Scala minore melodica di La", () => {
@@ -41,8 +41,8 @@ describe("scaleNames tests", () => {
       12,
       12
     );
-    const nomi = laMinoreMelodica.names("it").map((note) => note.name);
-    expect(nomi).toEqual(["La", "Si", "Do", "Re", "Mi", "Fa♯", "Sol♯"]);
+    const nomi = not251.names(laMinoreMelodica, ["it"]).map((note) => note.it);
+    expect(nomi).toEqual(["La", "Si", "Do", "Re", "Mi", "Fa#", "Sol#"]);
   });
 
   it("Scala minore melodica di La in notazione inglese", () => {
@@ -51,7 +51,7 @@ describe("scaleNames tests", () => {
       12,
       12
     );
-    const nomi = laMinoreMelodica.names().map((note) => note.name);
-    expect(nomi).toEqual(["A", "B", "C", "D", "E", "F♯", "G♯"]);
+    const nomi = not251.names(laMinoreMelodica).map((note) => note.en);
+    expect(nomi).toEqual(["A", "B", "C", "D", "E", "F#", "G#"]);
   });
 });
